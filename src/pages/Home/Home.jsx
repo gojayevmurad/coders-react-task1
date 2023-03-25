8cb416c608adb389d554a4e02c4e62d0743dfac2
Murad Gojayev <muradqocayevi20@gmail.com>
2023-03-25 23:36:14 +0400

    home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import boyImg from '../../assets/home_page-boy.png'
 import handDec from '../../assets/home_page-hand.png'
+import './style/home.css'
 
 const Home = () => {
     return (
@@ -12,13 +13,13 @@ const Home = () => {
                     </div>
                     <div className="home--desc">
                         <h1>
-                            Hey there 👋
-                            I am Amirhossein
+                            <p>Hey there 👋</p>
+                            <p>I am Amirhossein</p>
                         </h1>
                         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id magnam provident laudantium porro asperiores? Ipsam maxime vel itaque ullam corrupti, molestiae velit et quas cum eaque repudiandae tenetur officia sit ducimus quasi porro alias animi est. Assumenda dignissimos non placeat eaque. Totam quisquam maiores facere! Laboriosam hic nostrum repellendus est impedit in non sit ut.</p>
                         <div className="home--desc__actions">
                             <button className="about">About me</button>
-                            <button className="about">😮Let's see portfolios</button>
+                            <button className="portfolios">😮Let's see portfolios</button>
                         </div>
                     </div>
                     <div className="home-deco">
