22b99d7971feb5cbd1d27f3976923ae4d99d076f
Murad Gojayev <muradqocayevi20@gmail.com>
2023-03-26 12:09:04 +0400

    blog page

--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -1,8 +1,48 @@
 import React from 'react'
+import './style/blog.css'
+
+import blogLogo1 from '../../assets/blog_page.png'
+import blogLogo2 from '../../assets/blog_page(1).png'
+import blogLogo3 from '../../assets/blog_page(2).png'
+import blogLogo4 from '../../assets/blog_page(3).png'
+import blogLogo5 from '../../assets/blog_page(4).png'
+
+let data = [
+  [blogLogo1, 'There is more than you know', "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard"],
+  [blogLogo2, 'There is more than you know', "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard"],
+  [blogLogo3, 'There is more than you know', "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard"],
+  [blogLogo4, 'There is more than you know', "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard"],
+  [blogLogo5, 'There is more than you know', "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard"],
+]
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div className='blog'>
+      <div className="container">
+        <div className="blog--content">
+
+          {
+            data.map(item => {
+              return (
+                <div className="blog--item">
+                  <div className="blog--item__img">
+                    <img src={item[0]} alt="blog item" />
+                  </div>
+                  <div className="blog--item__text">
+                    <h3>{item[1]}</h3>
+                    <p>{item[2]}</p>
+                    <span>Read more<span class="material-symbols-outlined">
+                      read_more
+                    </span></span>
+                  </div>
+                </div>
+              )
+            })
+          }
+
+        </div>
+      </div>
+    </div>
   )
 }
 
