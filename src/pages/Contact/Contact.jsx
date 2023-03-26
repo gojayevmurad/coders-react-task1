42c659075a3b3c3f4a42861706cf4fcd2da204d1
Murad Gojayev <muradqocayevi20@gmail.com>
2023-03-26 11:29:24 +0400

    portfolios page

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
-import styles from './style/contact.module.css'
+import contactBoy from '../../assets/contact_page.png';
+import './style/contact.css'
+
+
 
 const Contact = () => {
   return (
-    <div className={styles.title}>Contact</div>
+    <div className="contact">
+      <div className="container">
+        <div className="contact--content">
+          <div className="contact--img">
+            <img src={contactBoy} alt="contact img" className='imgimg' />
+          </div>
+          <div className="contact--text">
+            <h2>You can reach me any time🙂</h2>
+            <p className='lorem'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur nobis dolores, eaque fuga at quasi, sint possimus eos, accusantium vero obcaecati? Ipsum accusantium eaque tenetur deleniti odio possimus iusto maxime.</p>
+            <p>Twitter: amirhwsin</p>
+            <p>Github: amirhwsin</p>
+            <p>Dribble: amirhwsin</p>
+            <p>Telegram: amirhwsin</p>
+            <p>Email: amirhwsin</p>
+            <div className="contact--text__actions">
+              <button className="about">About me</button>
+              <button className="portfolios">😮Let's see portfolios</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/pages/Portfolios/portfolios.jsx
+++ src/pages/Portfolios/portfolios.jsx
@@ -1,8 +1,54 @@
 import React from 'react'
+import './style/portfolio.css'
+import portfoliosLogo1 from '../../assets/portfolios_page.png'
+import portfoliosLogo2 from '../../assets/portfolios_page(1).png'
+import portfoliosLogo3 from '../../assets/portfolios_page(2).png'
+import portfoliosLogo4 from '../../assets/portfolios_page(3).png'
+import portfoliosLogo5 from '../../assets/portfolios_page(4).png'
+import portfoliosLogo6 from '../../assets/portfolios_page6.png'
 
+
+
+let portfoliosItems = [
+  [portfoliosLogo1, 'There is more than you know', "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard"],
+  [portfoliosLogo2, 'There is more than you know', "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard"],
+  [portfoliosLogo3, 'There is more than you know', "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard"],
+  [portfoliosLogo4, 'There is more than you know', "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard"],
+  [portfoliosLogo5, 'There is more than you know', "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard"],
+  [portfoliosLogo6, 'There is more than you know', "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard"]
+]
 const Portfolios = () => {
   return (
-    <div>Portfolios</div>
+    <div className='portfolios'>
+      <div className="container">
+        <div className="portfolios--content">
+          {
+            portfoliosItems.map(item => {
+              return (
+                <div className="portfolios--item">
+                  <div className="portfolios--item__img">
+                    <img src={item[0]} alt="portfolio" />
+                  </div>
+                  <div className="portfolios--item__content">
+                    <p className="portfolio__head">
+                      {item[1]}
+                    </p>
+                    <p className="portfolio__body">
+                      {item[2]}
+                    </p>
+                    <p className="portfolio__more">
+                      Read more<span class="material-symbols-outlined">
+                        read_more
+                      </span>
+                    </p>
+                  </div>
+                </div>
+              )
+            })
+          }
+        </div>
+      </div>
+    </div>
   )
 }
 
